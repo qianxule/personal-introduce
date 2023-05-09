e6fc56803a7e0e49704b90d58d2006abd7b56f02
qianxule <127176364+qianxule@users.noreply.github.com>
2023-05-09 18:24:17 +0800

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $("#topindex1").click(function(){
+    $("#leftside").fadeOut();
+});
+});
+
+$(document).ready(function(){
+    $("#topindex2").click(function(){
+    $("#leftside").fadeIn();
+});
+});
